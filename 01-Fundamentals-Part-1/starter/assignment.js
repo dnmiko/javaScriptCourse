17314af0a8c570911fc63ed65030b2ba23819010
Diego Cifuentes <dcifuentj@gmail.com>
2021-10-01 16:10:43 -0500

    JavaScript Fundamentals - Part 1. Values and variables

---
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -0,0 +1,7 @@
+let country = "Mexico";
+let continent = "America";
+let population = 126014024;
+
+console.log("Country: ", country);
+console.log("Continent: ", continent);
+console.log("Population: ", population);
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,6 @@
+let js = "amazing";
+if (js === "amazing") {
+  alert("JS is fun");
+}
+
+console.log(20 + 15 + 42);
