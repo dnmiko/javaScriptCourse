0ec8e0ee33a391a00503a710fbca4e0c341d1605
Diego Cifuentes <dcifuentj@gmail.com>
2021-10-01 16:40:57 -0500

    JavaScript Fundamentals - Part 1. Data types

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,6 +1,25 @@
+console.log("[Script.js]");
+/* Lesson 1 - Variables and Values
 let js = "amazing";
 if (js === "amazing") {
   alert("JS is fun");
 }
 
 console.log(20 + 15 + 42);
+*/
+
+let myNumber = 4;
+let myFloat = 4.5;
+let myString = "Hola";
+let myBool = false;
+let myUndef;
+
+console.log("Type of myNumber", typeof myNumber);
+console.log("Type of myFloat", typeof myFloat);
+console.log("Type of myString", typeof myString);
+console.log("Type of myBool", typeof myBool);
+console.log("Type of myUndef", typeof myUndef);
+
+myString = 4;
+
+console.log("Type of myString", typeof myString);
