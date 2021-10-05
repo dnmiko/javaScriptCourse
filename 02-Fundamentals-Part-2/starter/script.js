2592d437a572745fcfd186d7dff4c4f1d830014d
Diego Cifuentes <dcifuentj@gmail.com>
2021-10-05 06:32:48 -0500

    Lesson 2, Section 2 - Functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,7 +1,32 @@
 "use strict";
 
+console.log("[Script.js]");
+
+/* Lesson 1 - Strict mode 
 let hasDriversLicense = false;
 const passTest = true;
 
 if (passTest) hasDriversLicense = true; //Strict mode lifts an error because this variable does not exist
 if (hasDriversLicense) console.log("I can drive");
+*/
+
+/* Lesson 2 - Functions
+
+*/
+
+function logger() {
+  console.log("My name is Diego");
+}
+
+logger();
+
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice containing ${apples} apples and ${oranges} oranges`;
+  return juice;
+}
+
+const orangeJuice = fruitProcessor(0, 6);
+const fruitJuice = fruitProcessor(3, 4);
+
+console.log(orangeJuice);
+console.log(fruitJuice);
