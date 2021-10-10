54529af34cc5f091487d58f63a5a14f851dd1cf9
Diego Cifuentes <dcifuentj@gmail.com>
2021-10-10 17:39:41 -0500

    Finished the second project of the JS in the Browser section. Created a Modal project to handly events from the keyboard and to manipulate the css classes for given DOM elements

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,38 @@
 'use strict';
+
+//This time we are storing the different elements from the DOM in variables so we can use them many times.
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const closeModalBtn = document.querySelector('.close-modal');
+const showModalBtns = document.querySelectorAll('.show-modal');
+let showModal = false;
+
+const hideModal = function () {
+    if (showModal) {
+        showModal = false;
+        modal.classList.add('hidden');
+        overlay.classList.add('hidden');
+    }
+};
+
+const openModal = function () {
+    if (!showModal) {
+        showModal = true;
+        modal.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+    }
+};
+
+showModalBtns.forEach(button => {
+    button.addEventListener('click', openModal);
+});
+
+closeModalBtn.addEventListener('click', hideModal);
+
+overlay.addEventListener('click', hideModal);
+
+document.addEventListener('keydown', function (event) {
+    if (event.key === 'Escape') {
+        hideModal();
+    }
+});
