78ce5d4a0d077a77553a31ecdf6e5291e26e09c1
Diego Cifuentes <dcifuentj@gmail.com>
2021-10-20 20:05:58 -0500

    Section 6, Lesson 1. Default parameters for functions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const bookings = [];
+
+//Default parameters
+const createBooking = function (flightNum, numPassengers = 1, price = 500) {
+  //Old way to do default parameters
+  /*   numPassengers = numPassengers || 1;
+  price = price || 500; */
+
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+
+  bookings.push(booking);
+};
+
+createBooking('A340');
+createBooking('A340', 5);
+createBooking('A340', 5, 2500);
+console.log(bookings);
